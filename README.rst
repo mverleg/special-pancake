6fd66c789924765d44771d9fa71d36fd850af64a
Mark <mverleg.noreply@gmail.com>
2018-01-19 20:47:22 +0100

    standard demo with minimal adjustments to make it work

---
+++ README.rst
@@ -0,0 +1,11 @@
+
+Special Pancake
+===============================
+
+This is a little experiment with Libgdx.
+
+It is derived from the Libgdx demo Superjumper.
+
+The name was suggested by Github.
+
+
---
+++ android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.badlogicgames.superjumper.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogicgames.superjumper.SuperJumper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useWakelock = true;
+		initialize(new SuperJumper(), config);
+	}
+}
